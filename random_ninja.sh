7e3395d93ecdf93882f7f4603b0ddb8dade8b0fe
Hari haran <hariharan.uno@gmail.com>
2013-01-01 02:00:00 +0530

    A random commit message

---
+++ random_ninja.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+p=02002013
+finmonth=9
+findate=30
+zero=0
+z=10
+q=4
+e=$(( $RANDOM % 10 ));
+
+for (( month=01; $month<=$finmonth; month++ ))
+do
+    for (( date=01; $date<=$findate; date++ ))
+    do
+	if [ $date -lt $z ]
+	then
+	    if [ $month -lt $z ]
+	    then
+		combined=${zero}$month${zero}$date$p
+		date ${combined}
+	    else
+		combined=$month${zero}$date$p
+		date ${combined}
+	    fi
+	else
+	    if [ $month -lt $z ]
+	    then
+		combined=${zero}$month$date$p
+		date ${combined}
+	    else
+		combined=$month$date$p
+		date ${combined}
+	    fi
+	fi
+	r=$(( $RANDOM % 7 ));
+	if [ $e -gt $q ]
+	then
+	for (( i=0; i<=${r}; i++ ))
+	do
+	    echo "This is:" `sh ./random_word.sh` >> random.txt;  git add -A; git commit -m "A random commit message"
+	done
+	fi
+	e=$(( $RANDOM % 10 ));
+    done
+done
---
+++ random_word.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+perl -e 'open IN, "</usr/share/dict/words";rand($.) < 1 && ($n=$_) while <IN>;print $n'
